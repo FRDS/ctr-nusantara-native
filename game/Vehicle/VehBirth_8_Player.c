e61bfeac93479a95c532ddc9ed8798953cea566e
Abdulrazaq Alhendi <dev@aalhendi.com>
2026-05-26 07:40:10 +0300

    fix(vehicle): verify player birth

--- game/Vehicle/VehBirth_8_Player.c
+++ game/Vehicle/VehBirth_8_Player.c
@@ -1,8 +1,9 @@
 #include <common.h>
 
+// NOTE(aalhendi): ASM-verified NTSC-U 926 0x80058ec0-0x80058f54.
 struct Driver *VehBirth_Player(int index)
 {
-	struct Thread *t = PROC_BirthWithObject(0x62c0100, 0, 0, 0);
+	struct Thread *t = PROC_BirthWithObject(0x62c0100, 0, sdata->s_player, 0);
 
 	struct Driver *d = t->object;
 	memset(d, 0, 0x62c);
--- game/Vehicle/VehCalc_0_InterpBySpeed.c
+++ game/Vehicle/VehCalc_0_InterpBySpeed.c
@@ -1,5 +1,6 @@
 #include <common.h>
 
+// NOTE(aalhendi): ASM-verified NTSC-U 926 0x80058f54-0x80058f9c.
 int VehCalc_InterpBySpeed(int val, int speed, int desired)
 {
 	if (val > desired)
