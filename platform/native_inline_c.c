fdbd67ea25e493ed318fff3966c01f94cfd99e64
Abdulrazaq Alhendi <dev@aalhendi.com>
2026-06-04 14:01:27 +0300

    refactor(gte): own inline register facade

--- platform/native_inline_c.c
+++ platform/native_inline_c.c
@@ -1,11 +1,20 @@
-#include "psx/inline_c.h"
-#include "psx/libgte.h"
-#include "psx/gtereg.h"
-#include "../gte/PsyX_GTE.h"
+/*
+ * Derived from REDRIVER2/PsyCross MIT source:
+ * externals/PsyCross/src/psx/INLINE_C.C
+ * See THIRD_PARTY_NOTICES.md for copyright and license details.
+ */
 
-unsigned int MFC2(int reg)
+#include <macros.h>
+#include <psx/gtereg.h>
+#include <psx/inline_c.h>
+#include <psx/libgte.h>
+
+extern u32 gte_leadingzerocount(u32 lzcs);
+
+u32 MFC2(s32 reg)
 {
-	switch (reg) {
+	switch (reg)
+	{
 	case 1:
 	case 3:
 	case 5:
@@ -13,7 +22,7 @@ unsigned int MFC2(int reg)
 	case 9:
 	case 10:
 	case 11:
-		gteRegs.CP2D.p[reg].d = (int)gteRegs.CP2D.p[reg].sw.l;
+		gteRegs.CP2D.p[reg].d = (s32)gteRegs.CP2D.p[reg].sw.l;
 		break;
 
 	case 7:
@@ -21,7 +30,7 @@ unsigned int MFC2(int reg)
 	case 17:
 	case 18:
 	case 19:
-		gteRegs.CP2D.p[reg].d = (unsigned int)gteRegs.CP2D.p[reg].w.l;
+		gteRegs.CP2D.p[reg].d = (u32)gteRegs.CP2D.p[reg].w.l;
 		break;
 
 	case 15:
@@ -37,10 +46,11 @@ unsigned int MFC2(int reg)
 	return gteRegs.CP2D.p[reg].d;
 }
 
-int MFC2_S(int reg)
+s32 MFC2_S(s32 reg)
 {
-	// FIXME: Is that modifiers should be signed too?
-	switch (reg) {
+	// TODO(aalhendi): Confirm whether modifier reads should also use signed semantics.
+	switch (reg)
+	{
 	case 1:
 	case 3:
 	case 5:
@@ -48,7 +58,7 @@ int MFC2_S(int reg)
 	case 9:
 	case 10:
 	case 11:
-		gteRegs.CP2D.p[reg].d = (int)gteRegs.CP2D.p[reg].sw.l;
+		gteRegs.CP2D.p[reg].d = (s32)gteRegs.CP2D.p[reg].sw.l;
 		break;
 
 	case 7:
@@ -56,7 +66,7 @@ int MFC2_S(int reg)
 	case 17:
 	case 18:
 	case 19:
-		gteRegs.CP2D.p[reg].d = (unsigned int)gteRegs.CP2D.p[reg].w.l;
+		gteRegs.CP2D.p[reg].d = (u32)gteRegs.CP2D.p[reg].w.l;
 		break;
 
 	case 15:
@@ -72,8 +82,10 @@ int MFC2_S(int reg)
 	return gteRegs.CP2D.p[reg].sd;
 }
 
-void MTC2(unsigned int value, int reg) {
-	switch (reg) {
+void MTC2(u32 value, s32 reg)
+{
+	switch (reg)
+	{
 	case 15:
 		C2_SXY0 = C2_SXY1;
 		C2_SXY1 = C2_SXY2;
@@ -97,8 +109,10 @@ void MTC2(unsigned int value, int reg) {
 	gteRegs.CP2D.p[reg].d = value;
 }
 
-void MTC2_S(int value, int reg) {
-	switch (reg) {
+void MTC2_S(s32 value, s32 reg)
+{
+	switch (reg)
+	{
 	case 15:
 		C2_SXY0 = C2_SXY1;
 		C2_SXY1 = C2_SXY2;
@@ -122,8 +136,10 @@ void MTC2_S(int value, int reg) {
 	gteRegs.CP2D.p[reg].sd = value;
 }
 
-void CTC2(unsigned int value, int reg) {
-	switch (reg) {
+void CTC2(u32 value, s32 reg)
+{
+	switch (reg)
+	{
 	case 4:
 	case 12:
 	case 20:
@@ -131,7 +147,7 @@ void CTC2(unsigned int value, int reg) {
 	case 27:
 	case 29:
 	case 30:
-		value = (int)(short)value;
+		value = (u32)(s32)(s16)value;
 		break;
 
 	case 31:
@@ -144,8 +160,10 @@ void CTC2(unsigned int value, int reg) {
 	gteRegs.CP2C.p[reg].d = value;
 }
 
-void CTC2_S(int value, int reg) {
-	switch (reg) {
+void CTC2_S(s32 value, s32 reg)
+{
+	switch (reg)
+	{
 	case 4:
 	case 12:
 	case 20:
@@ -153,7 +171,7 @@ void CTC2_S(int value, int reg) {
 	case 27:
 	case 29:
 	case 30:
-		value = (int)(short)value;
+		value = (s32)(s16)value;
 		break;
 
 	case 31:
@@ -166,21 +184,21 @@ void CTC2_S(int value, int reg) {
 	gteRegs.CP2C.p[reg].sd = value;
 }
 
-unsigned int CFC2(int reg)
+u32 CFC2(s32 reg)
 {
-	// TODO: correct functionality
+	// TODO(aalhendi): Verify CFC2 control-register edge cases against PS1 GTE behavior.
 
 	return gteRegs.CP2C.p[reg].d;
 }
 
-int CFC2_S(int reg)
+s32 CFC2_S(s32 reg)
 {
-	// TODO: correct functionality
+	// TODO(aalhendi): Verify CFC2 control-register edge cases against PS1 GTE behavior.
 
 	return gteRegs.CP2C.p[reg].sd;
 }
 
-int doCOP2(int op)
+s32 doCOP2(s32 op)
 {
 	return GTE_operator(op);
 }
